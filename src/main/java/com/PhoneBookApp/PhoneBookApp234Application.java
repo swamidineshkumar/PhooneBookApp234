e87a8799f475d2893675944257e356ece2e1703e
swamiji1 <dineshswami94414@gmail.com>
2022-06-18 13:36:10 +0530

    PHN 2 create SpringBootApp with dependencies

---
+++ src/main/java/com/PhoneBookApp/PhoneBookApp234Application.java
@@ -0,0 +1,13 @@
+package com.PhoneBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApp234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApp234Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/PhoneBookApp/PhoneBookApp234ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PhoneBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApp234ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
